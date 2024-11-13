29dce2d19ecafe420704c8c596638638e149c408
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-11-12 18:48:29 -0600

    header and enemies + mario assets

---
+++ src/config.py
@@ -0,0 +1,6 @@
+'''
+ # @ Author: Niels Ouvrard - Diego Jiménez Ontiveros - Santiago Arreola Munguia
+ # @ Create Time: 2024-11-12 18:36:01
+ # @ Description:
+ '''
+
--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+'''
+ # @ Author: Niels Ouvrard - Diego Jiménez Ontiveros - Santiago Arreola Munguia
+ # @ Create Time: 2024-11-12 17:26:06
+ # @ Description:
+ '''
+
 import pygame
 
 
